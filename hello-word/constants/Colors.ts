4d862e347d1d2c25cd5281bdcba2a41068fb2b27
pawelataman <pawel.atmn@gmail.com>
2024-09-16 23:03:13 +0200

    Add colors

--- hello-word/constants/Colors.ts
+++ hello-word/constants/Colors.ts
@@ -1,26 +1,14 @@
-/**
- * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
- * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
- */
-
-const tintColorLight = '#0a7ea4';
-const tintColorDark = '#fff';
 
 export const Colors = {
   light: {
-    text: '#11181C',
-    background: '#fff',
-    tint: tintColorLight,
-    icon: '#687076',
-    tabIconDefault: '#687076',
-    tabIconSelected: tintColorLight,
+    green: '#8AC843',
+    yellow: '#FFF200',
+    white: "#FFFFFF",
+    red: '#FF4747',
+    bgLight: '#F5F5F5',
+    blue: '#2683EF',
+    textLight: '#878787',
+    darkGreen: '#008227'
   },
-  dark: {
-    text: '#ECEDEE',
-    background: '#151718',
-    tint: tintColorDark,
-    icon: '#9BA1A6',
-    tabIconDefault: '#9BA1A6',
-    tabIconSelected: tintColorDark,
-  },
+
 };
