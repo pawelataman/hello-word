8c96b090584586a9520cddb817384ba7d2c6f85f
pawelataman <pawel.atmn@gmail.com>
2024-10-08 00:02:48 +0200

    Add Svg handling

--- hello-word/app/_layout.tsx
+++ hello-word/app/_layout.tsx
@@ -1,9 +1,26 @@
-import { Stack } from "expo-router";
+import { SplashScreen, Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "InterTight-Bold": require("@/assets/fonts/InterTight-Bold.ttf"),
+    "InterTight-Medium": require("@/assets/fonts/InterTight-Medium.ttf"),
+    "InterTight-Regular": require("@/assets/fonts/InterTight-Regular.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
 }
