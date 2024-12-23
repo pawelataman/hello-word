edfe0bdce449ec70e63c24e9f3e46228292d96b6
pawelataman <pawel.atmn@gmail.com>
2024-12-23 15:46:36 +0100

    Reforat

--- api/src/core/auth/auth.middleware.ts
+++ api/src/core/auth/auth.middleware.ts
@@ -1,12 +1,12 @@
-import {ForbiddenException, Injectable, NestMiddleware} from '@nestjs/common';
-import {NextFunction, Request, Response} from 'express';
+import { ForbiddenException, Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
 
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-    use(req: Request, res: Response, next: NextFunction) {
-        if (!req['auth']?.userId) {
-            return next(new ForbiddenException())
-        }
-        next();
+  use(req: Request, res: Response, next: NextFunction) {
+    if (!req["auth"]?.userId) {
+      return next(new ForbiddenException());
     }
+    next();
+  }
 }
