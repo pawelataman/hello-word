010136d2101a42254bf8cfe46b9a4a08286ac3e5
pawelataman <pawel.atmn@gmail.com>
2024-12-24 01:53:03 +0100

    User profile

--- hello-word/app/(home)/main/profile.tsx
+++ hello-word/app/(home)/main/profile.tsx
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { useUser } from '@clerk/clerk-expo';
 
 export default function() {
+	const { user } = useUser();
 	return (
-		<View className={'flex-1 bg-gray-200'}>
-			<Text> </Text>
+		<View className={'flex-1 bg-gray-200 p-4'}>
+			<View className="w-full gap-4 items-center">
+				<Image className="w-32 h-32 rounded-full border-2 border-green-100"
+					   src={user?.imageUrl} alt="Bordered avatar" />
+				<Text className="font-semibold text-xl">{user?.emailAddresses[0].emailAddress}</Text>
+			</View>
+
 		</View>
 	);
 }
