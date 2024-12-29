df62b21b5253ef00c7896e505be3b606d41d6a14
pawelataman <pawel.atmn@gmail.com>
2024-12-29 19:38:31 +0100

    Improve hearing mode

--- hello-word/components/quiz/QuizQuestion.tsx
+++ hello-word/components/quiz/QuizQuestion.tsx
@@ -13,7 +13,7 @@ interface QuizQuestionProps {
 }
 
 export default function({ question, mode }: QuizQuestionProps) {
-	const { quizLanguage, setAnsweringEnabled } = useQuizStore();
+	const { quizLanguage, setAnsweringEnabled, currentQuestionStatus } = useQuizStore();
 	const { getQuestionLabel } = useQuizTranslation();
 
 	useEffect(() => {
@@ -22,17 +22,19 @@ export default function({ question, mode }: QuizQuestionProps) {
 
 	const showPlayback: boolean = quizLanguage?.code === LANG_CODE.PL;
 	const showQuestion: boolean = mode !== 'hearing';
+	const questionAnswered: boolean = currentQuestionStatus === 'answered';
 
 	const enableAnswering = () => setAnsweringEnabled(true);
 
 	return (
-		<View className="m-5 py-16 px-2.5 rounded-lg bg-gray-100 h-auto gap-5 items-center justify-evenly">
-			{!showPlayback && showQuestion &&
+		<View className="m-5 py-16 px-2.5 rounded-lg bg-gray-100 h-48 gap-5 items-center justify-evenly">
+			{(showQuestion || questionAnswered) &&
 				<Text className="text-center font-bold text-4xl text-gray-900">
 					{getQuestionLabel(question!)}
 				</Text>
 			}
-			{showPlayback && <PlaybackWord word={question!.en} lang="en" onDone={enableAnswering} />}
+			{(!questionAnswered && showPlayback) &&
+				<PlaybackWord word={question!.en} lang="en" onDone={enableAnswering} />}
 		</View>
 	);
 }
