43f74e95c967943c3e3b7451e7b0a2f3ed026825
pawelataman <pawel.atmn@gmail.com>
2025-03-26 21:41:29 +0100

    chore: migrate on start

--- api/migrate.sh
+++ api/migrate.sh
@@ -1,1 +1,1 @@
-GOOSE_DRIVER=postgres GOOSE_DBSTRING="${DB_URL}/${DB_NAME}" goose up -dir ./internal/db/sql/migrations
+DB_NAME=pasdasd GOOSE_DRIVER=postgres GOOSE_DBSTRING="${DB_URL}/${DB_NAME}" goose up -dir ./internal/db/sql/migrations
